f0fa94a101e4db95625bfb674fd23bf63e7a0595
Toni Guli <pllumiguli@icloud.com>
2021-06-26 19:15:37 +0200

    implemented monty_open_door() and edited check_if_valid() to allow inputing the data as a slice

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 enum State {
 	Closed,
@@ -29,22 +30,31 @@ impl Door {
 }
 
 fn main() {
-    println!("Hello, world!");
 	let mut doors: [Door; 3] = [Door::new(), Door::new(), Door::new()];
-	let mut choice: u8 = 0;
+	
+	let mut choice: usize = 0;
+	
+	// put the prize in one door
+	doors[select_door(doors.len())].has_prize = true;
 
+	// Let the user choose their door
 	loop {
 		println!("Which door do you choose? (1, 2, 3)");
 		read_input(&mut choice);
 		
-		if check_if_valid(&choice) {
+		if check_if_valid(&choice, &[1, 2, 3]) {
 			break;
 		}
 		println!("Invalid number entered!");
 	}
+
+	// Open 1 door
+	monty_open_door(&mut doors);
+
+
 }
 
-fn read_input(data: &mut u8) {
+fn read_input(data: &mut usize) {
 	let mut raw_data = String::new();
 	io::stdin().read_line(&mut raw_data).expect("Error while reading from stdin!");
 
@@ -53,10 +63,28 @@ fn read_input(data: &mut u8) {
 				.expect("Unable to parse inputed data!");
 }
 
-fn check_if_valid(data: &u8) -> bool {
-	if *data >=1u8 && *data <= 3u8 {
-		true
-	} else {
-		false
+fn check_if_valid(data: &usize, range: &[usize]) -> bool {
+	let mut out: bool = false;
+
+	for element in range {
+		if *data == *element {
+			out = true;
+		}
+	}
+	out
+}
+
+fn select_door(max_index: usize) -> usize {
+	rand::thread_rng().gen_range(0..max_index) as usize
+}
+
+fn monty_open_door(doors: &mut [Door]) {
+	loop {
+		let index = select_door(doors.len());
+		
+		if !doors[index].is_open() && !doors[index].has_prize {
+			doors[index].open();
+			break;
+		}
 	}
 }
